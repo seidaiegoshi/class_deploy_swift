b98984a339e1de7babe36f10aaa8aeac8164bf0d
seidaiegoshi <egoshi.seidai@gmail.com>
2023-02-05 11:30:48 +0900

    Initial Commit

---
+++ Omikuji/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Omikuji
+//
+//  Created by 江越正大 on 2023/02/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Omikuji/OmikujiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmikujiApp.swift
+//  Omikuji
+//
+//  Created by 江越正大 on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct OmikujiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
