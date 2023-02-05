d6a6d8bc86e322a36473d6bad163fc318f46fd84
seidaiegoshi <egoshi.seidai@gmail.com>
2023-02-05 12:49:54 +0900

    じゃんけん

--- Omikuji/ContentView.swift
+++ Omikuji/ContentView.swift
@@ -1,49 +1,85 @@
-//
-//  ContentView.swift
-//  Omikuji
-//
-//  Created by 江越正大 on 2023/02/05.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    @State var imageName = "omikuji"
-    
+    @State var result = "結果"
+    @State var imageName = "janken"
     var body: some View {
-        VStack(spacing:32){
+        VStack {
             Image(imageName)
                 .resizable()
                 .frame(maxWidth: .infinity)
-            HStack(spacing:16){
-                Button("リセット") {
-                    
-                }
-                .frame(maxWidth: .infinity, minHeight: 50)
-                .background(.red)  //背景色を赤に
-                .foregroundColor(.white)
-                // ここまで
-                Button("おみくじを引く") {
-                    updateOmikujiImageView()
+            HStack {
+                Button("ぐー") {
+                    showResult(myHand: .gu)
                 }
                 .frame(maxWidth: .infinity, minHeight: 50) //サイズ
                 .background(.green)  //背景のViewを設定するModifier, ColorというViewのgreenを指定(型推論でColorを省略)
-                .foregroundColor(.white) //前面に表示されるView(ここでは"おみくじを引く"というText)の色を設定できるModi
+                .foregroundColor(.white)
+                Button("ちょき") {
+                    showResult(myHand: .choki)
+                }
+                .frame(maxWidth: .infinity, minHeight: 50) //サイズ
+                .background(.red)  //背景のViewを設定するModifier, ColorというViewのgreenを指定(型推論でColorを省略)
+                .foregroundColor(.white)
+                Button("ぱー") {
+                    showResult(myHand: .pa)
+                }
+                .frame(maxWidth: .infinity, minHeight: 50) //サイズ
+                .background(.blue)  //背景のViewを設定するModifier, ColorというViewのgreenを指定(型推論でColorを省略)
+                .foregroundColor(.white)
+            }
+            Text(result)
+        }
+        .padding()
+    }
+
+    func showResult(myHand: JankenHand) {
+        let comHand = JankenHand(rawValue: Int.random(in: 0...2))!
+        imageName = comHand.imageName
+        if myHand == .gu {
+            if comHand == .gu {
+                result = "引き分け"
+            } else if comHand == .choki {
+                result = "勝ち"
+            } else {
+                result = "負け"
+            }
+        } else if myHand == .choki {
+            if comHand == .gu {
+                result = "負け"
+            } else if comHand == .choki {
+                result = "引き分け"
+            } else {
+                result = "勝ち"
+            }
+        } else {
+            if comHand == .gu {
+                result = "勝ち"
+            } else if comHand == .choki {
+                result = "負け"
+            } else {
+                result = "引き分け"
             }
         }
-        .padding(40)
-    }
-    
-    func updateOmikujiImageView(){
-        let lucks = ["daikichi","kichi","kyou"]
-        imageName = lucks[Int.random(in: 0...2)]
     }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+enum JankenHand: Int, Equatable {
+    case gu, choki, pa
+    var imageName: String {
+        switch self {
+        case .gu:
+            return "gu"
+        case .choki:
+            return "choki"
+        case .pa:
+            return "pa"
+        }
     }
 }
